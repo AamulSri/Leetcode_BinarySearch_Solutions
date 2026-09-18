a5e8b323b84d05ffb6b940e8c32d313a847a7381
AamulSri <aamul24.vns@gmail.com>
2026-09-18 20:54:03 +0530

    UpperBound code

---
+++ java/src/Strivers_UpperBound.java
@@ -0,0 +1,25 @@
+public class Strivers_UpperBound {
+
+    public static void main(String[] args) {
+        System.out.println(lowerBound(new int[]{2,3,7,10,11,11,25}, 11));
+    }
+
+    static int lowerBound(int[] arr, int target) {
+        // code here
+        int start =0;
+        int end = arr.length-1;
+
+        while(start <= end ){
+            int mid = start + ( end -start)/2;
+
+            if( target >= arr[mid] ){
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+        }
+
+        return start;
+    }
+}
